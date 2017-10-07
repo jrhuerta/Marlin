a2dbc23b06d25b2632e59513412fca52bfa76172
Joaquin A. Rodriguez Huerta <jrhuerta@alayacare.com>
2017-10-07 16:44:28 -0400

    Z offset adjust.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -664,7 +664,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 25  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 0   // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.25   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.25   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 6000
